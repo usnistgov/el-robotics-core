987f7991200938aed2fb3f4ec5b7e000292f6d00
anthony.pietro <anthony.pietromartire@nist.gov>
2013-01-24 14:00:54 -0500

    -Cleaned the folder -Added "back pointers" with the help of HermiT (reasoner)
    Former-commit-id: b6a9425b7dc8620e7df5459c4297618f755fd1e4

---
+++ Generator/OwlToSql/HermiT/project/test/org/semanticweb/HermiT/tableau/AllTests.java
@@ -0,0 +1,26 @@
+package org.semanticweb.HermiT.tableau;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Test for the HermiT internals (tableau)");
+        //$JUnit-BEGIN$
+        suite.addTestSuite(TupleIndexTest.class);
+        suite.addTestSuite(TupleTableFullIndexTest.class);
+        suite.addTestSuite(DLClauseEvaluationTest.class);
+        suite.addTestSuite(DependencySetTest.class);
+        suite.addTestSuite(NIRuleTest.class);
+        suite.addTestSuite(MergeTest.class);
+        //$JUnit-END$
+        return suite;
+    }
+
+    public static void main(String... args) throws Throwable {
+        junit.textui.TestRunner.run(suite());
+        System.exit(0);
+    }
+
+}
--- Generator/Test/Debug/DataThing.d
+++
@@ -1,7 +0,0 @@
-DataThing.d: ../DataThing.cpp ../DataThing.h ../DAO.h ../Connection.h
-
-../DataThing.h:
-
-../DAO.h:
-
-../Connection.h:

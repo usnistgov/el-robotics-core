12ddc580358c414e36cb82c7af263f26887557fa
Stephen Balakirsky <stephen.balakirsky@gtri.gatech.edu>
2013-06-26 15:40:34 -0400

    Files for generating dataset added.
    Signed-off-by: Stephen Balakirsky <stephen.balakirsky@gtri.gatech.edu>
    
    Former-commit-id: 448729954342e594e97cd388d6629f5e4b23d69c

--- Simulation/Arm Motion/README.txt
+++ Simulation/Arm Motion/README.txt
@@ -1,4 +1,10 @@
 This directory contains data from arm motion attempts that utilized the ROS
 Fourte arm motion stack.Histogram.png and datafile1.dat contain random motion
 attempts with the default settings on the arm wizzard.
-Histogram2 contains data from the arm wizzard using the most complex solutions.
+Histogram2 and datafile2.dat contain data from the arm wizzard using the most complex solutions.
+randCommand.txt is the canonical command file used to generate both datasets.
+genPts.py is the python file that generated randCommand.txt
+
+to recollect the data from the simulator, modifications to rosInf.cpp need to be made--uncomment the marked sections of code 
+in navigationDoneCallback, and comment out the exit command.
+The global position frame needs to be changed to /base_link instead of /odom (the call to nextGoal.setGlobalFrame() in addArmGoal) .
---
+++ Simulation/Arm Motion/genPts.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import random
+import math
+
+num_goals = 50
+
+def getRandCont(randMin, randMax):
+  return random.random() * (randMax - randMin) + randMin
+
+def getUnitVector(vec):
+  vecMag = math.sqrt(sum([x**2 for x in vec]))
+  unitVec = [x/vecMag for x in vec]
+  return unitVec
+
+def getRandomUnitVector():
+  vec = [getRandCont(-1, 1) for i in range(3)]
+  return getUnitVector(vec)
+
+def getRandomPerp(vec):
+  other = getRandomUnitVector()
+  dotProduct = sum([x * y for (x,y) in zip(vec, other)])
+  
+  perp = [x - dotProduct * y for (x, y) in zip(other, vec)]
+  return getUnitVector(perp)
+
+def trunc(n, f):
+  slen = len('%.*f' % (n, f))
+  return str(f)[:slen]
+  
+outFile = open('randCommand.txt', 'w')
+outFile.write("InitCanon()\n")
+for index in range(num_goals):
+  pos = [getRandCont(-2.3,2.3), getRandCont(-2.3,2.3), getRandCont(-2.3,0)]
+  
+  zAxis = getRandomUnitVector()
+  xAxis = getRandomPerp(zAxis)
+  
+  line = ("MoveTo({{"+trunc(3,pos[0]) + "," +  trunc(3,pos[1]) + "," + trunc(3,pos[2]) + "},{" + 
+    trunc(3,zAxis[0]) + "," + trunc(3,zAxis[1]) + "," + trunc(3,zAxis[2]) + "},{" + 
+    trunc(3,xAxis[0]) + "," + trunc(3,xAxis[1]) + "," + trunc(3,xAxis[2]) + "}})\n")
+  outFile.write(line)
+outFile.write("EndCanon(36)\n")
+outFile.close()
+    

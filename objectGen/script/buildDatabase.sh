f8c0ee8be628b9927b21da64ea7e86e6ac3faf18
Teddy Weisman <theodore.weisman@nist.gov>
2013-07-24 14:06:20 -0400

    Added script to build database from kittingInstances.owl in one step
    Former-commit-id: 59fb18ecda2d103f16d6808d595637b13c32208c

---
+++ objectGen/script/buildDatabase.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#run this script to create a database from the kittingInstances.owl file in one step
+#mysql environment variables must be set in order for this to work
+
+IPMAS_DIRECTORY=../../
+
+if [ $# = 1 ]
+then
+	java -jar $IPMAS_DIRECTORY/Generator/generator_no_gui.jar -owl $1 -sql ./
+	echo Done generating sql.
+	echo Resetting database...
+	mysql -u $MysqlDefaultUser -h $MysqlDefaultHost --password=$MysqlDefaultPassword -e "DROP DATABASE $MysqlDefaultSchema;"
+	mysql -u $MysqlDefaultUser -h $MysqlDefaultHost --password=$MysqlDefaultPassword -e "CREATE DATABASE $MysqlDefaultSchema;"
+	echo Database reset.
+	echo Creating tables...
+	mysql -u $MysqlDefaultUser -h $MysqlDefaultHost --password=$MysqlDefaultPassword $MysqlDefaultSchema < kittingInstances.owlCreateTable.sql
+	echo Inserting table data...
+	mysql -u $MysqlDefaultUser -h $MysqlDefaultHost --password=$MysqlDefaultPassword $MysqlDefaultSchema < kittingInstances.owlInsertInto.sql
+	echo Done.
+else
+	echo "buildDatabase.sh:\ncreates a database using mysql environment variables and populates it using an owl instance file"
+	echo usage:
+	echo ./buildDatabase.sh path/to/owl.owl
+fi

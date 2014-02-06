5a14475c54a2ed2823e068fac3e6565fa7bb604d
zeidk <zeid.kootbally@nist.gov>
2014-02-06 15:20:08 -0500

    Added new versions of soapInstances.xml, soap.xsd, soapClasses.owl, and soapInstances.owl
    Former-commit-id: 089b54d4e5961d34a84389b64bcead82444d46c8

---
+++ kittingSystemRunner/kitting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo $PATH_INIT_FILE
+
+exit #  The right and proper method of "exiting" from a script.
+     #  A bare "exit" (no parameter) returns the exit status
+     #+ of the preceding command. 

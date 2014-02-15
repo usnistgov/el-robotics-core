75d8c6cb9dd383f514f310fee7e262eb7b1c1275
zeidk <zeid.kootbally@nist.gov>
2014-02-15 10:46:59 -0500

    Added the bash script kittingSystemRunner.sh
    Former-commit-id: 7f5db4addcb40061b0dda5b55d6c7fdbd6a78311

--- kittingSystemRunner/kittingSystemRunner.sh
+++ kittingSystemRunner/kittingSystemRunner.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-#Generate domain and problem files
-java -jar $PATH_OWL2PDDL_FILE
-#sleep 5
+#if owl2pddl.jar exists
+if [ -f $PATH_OWL2PDDL_FILE ];
+then
+   #Generate domain and problem files
+   java -jar $PATH_OWL2PDDL_FILE
+else
+   echo "File $PATH_OWL2PDDL_FILE does not exist... exits"
+   exit 2;
+fi
 
+#wait for the domain and problem files to be generated
+sleep 5
+
+#check if the domain file exists
 [ ! -f "$PATH_DOMAIN_OUTPUT_FILE" ] && { echo "Error: $PATH_DOMAIN_OUTPUT_FILE file not found."; exit 2; }
+#check if the problem file exists
 [ ! -f "$PATH_PROBLEM_OUTPUT_FILE" ] && { echo "Error: $PATH_PROBLEM_OUTPUT_FILE file not found."; exit 2; }
 
-
+#if the domain file exists and is not empty
 if [ -s "$PATH_DOMAIN_OUTPUT_FILE" ]
   then
+    #if the problem file exists and is not empty
     if [ -s "$PATH_PROBLEM_OUTPUT_FILE" ]
       then
+	#go to the planner directory
 	cd $PLANNER_DIRECTORY
 	#Run the plan
 	./plan $PATH_DOMAIN_OUTPUT_FILE $PATH_PROBLEM_OUTPUT_FILE $PATH_GENERATED_PLAN_FILE >/dev/null 2>/dev/null
+	#wait for the plan to be generated
 	sleep 5
       else
       echo "$PATH_PROBLEM_OUTPUT_FILE is empty."
@@ -25,17 +39,28 @@ if [ -s "$PATH_DOMAIN_OUTPUT_FILE" ]
 	exit 2;
 fi
 
+#check if $PATH_INPUT_PDDL_PLAN_FILE exists and is not empty
 [ ! -f "$PATH_INPUT_PDDL_PLAN_FILE" ] && { echo "Error: $PATH_INPUT_PDDL_PLAN_FILE file not found."; exit 2; }
 if [ -s "$PATH_INPUT_PDDL_PLAN_FILE" ]
   then
-    # Clean the generated plan file
-    java -jar $PATH_PLAN_CLEANER_FILE
-    sleep 5
+  #if pddlcleaner.jar exists
+  if [ -f $PATH_PLAN_CLEANER_FILE ];
+    then
+      # Clean the generated plan file
+      java -jar $PATH_PLAN_CLEANER_FILE
+      # wait for the plan to be cleaner
+      sleep 5
+    else
+      echo "File $PATH_PLAN_CLEANER_FILE does not exist... exits"
+      exit 2;
+  fi
+
   else
 	echo "$PATH_INPUT_PDDL_PLAN_FILE is empty."
 	exit 2;
 fi
 
+#check if $PATH_OUTPUT_PDDL_PLAN_FILE exists and is not empty
 [ ! -f "$PATH_OUTPUT_PDDL_PLAN_FILE" ] && { echo "Error: $PATH_OUTPUT_PDDL_PLAN_FILE file not found."; exit 2; }
 if [ -s "$PATH_OUTPUT_PDDL_PLAN_FILE" ]
   then

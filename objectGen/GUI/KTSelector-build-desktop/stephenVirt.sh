acd5e61c55101bd2d90ddfc3cdff7a44b2671af5
Stephen Balakirsky <stepen.balakirsky@gtri.gatech.edu>
2014-04-10 07:36:52 -0400

    Test files.
    Signed-off-by: Stephen Balakirsky <stepen.balakirsky@gtri.gatech.edu>
    
    Former-commit-id: 62350bdde72998106a9bca2ce7e73c2bdca76f9e

---
+++ objectGen/GUI/KTSelector-build-desktop/stephenVirt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+roslaunch usarsim_inf usarsimRobot.launch coordinate_change:=True hostname:=10.108.21.180&
+#roslaunch usarsim_inf usarsimKR60.sbalakirsky.launch &
+#roslaunch usarsim_inf usarsimRobot.launch hostname:=sbalakirsky-e64&
+sleep 5;
+roslaunch KR60_arm_navigation KR60_arm_navigation.launch &
+sleep 5;
+rosrun usarsim_tools collision_map.py 
+

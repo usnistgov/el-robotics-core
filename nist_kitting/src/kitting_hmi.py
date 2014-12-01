461ee4a270c7321d2c29358c1d15b558319ddfdf
Fred Proctor <frederick.proctor@nist.gov>
2014-12-01 11:25:29 -0500

    Used grid manager for kitting HMI

--- nist_kitting/src/kitting_hmi.py
+++ nist_kitting/src/kitting_hmi.py
@@ -27,8 +27,63 @@ if DEBUG:
     print "kitting_hmi: host:", HOST
     
 class App:
+    MSG_LEN = 1000
+    PERIOD = 5
 
     def __init__(self, master):
+        self.connected = False
+        self.nameVar = StringVar()
+        self.portVar = StringVar()
+        self.kitVar = StringVar()
+        self.cmdVar = StringVar()
+        self.statusVar = StringVar()
+        self.hbVar = StringVar()
+        self.nameVar.set("localhost")
+        self.portVar.set("6006")
+        self.kitVar.set("kit.kit")
+        self.cmdVar.set("(none)")
+        self.statusVar.set("(none)")
+        self.hbVar.set("0")
+
+        #
+        Label(master, text="Host").grid(row=0,column=0,columnspan=2)
+        # #
+        Label(master, text="Name:").grid(row=1,column=0)
+        Entry(master, textvariable=self.nameVar).grid(row=1,column=1)
+        # #
+        Label(master, text="Port:").grid(row=2,column=0)
+        Entry(master, textvariable=self.portVar).grid(row=2,column=1)
+        # #
+        Button(master, text="Connect", command=self.connect).grid(row=3,column=0)
+        Button(master, text="Disconnect", command=self.disconnect).grid(row=3,column=1)
+
+        #
+        Label(master, text="Kit").grid(row=0, column=2, columnspan=2)
+        # #
+        Label(master, text="Name:").grid(row=1, column=2)
+        Entry(master, textvariable=self.kitVar).grid(row=1, column=3)
+        # #
+        Button(master, text="Browse...", command=self.browse).grid(row=3,column=2)
+
+        #
+        Label(master, text="Status").grid(row=0, column=4, columnspan=2)
+        # #
+        Label(master, text="Command:").grid(row=1, column=4)
+        Entry(master, textvariable=self.cmdVar).grid(row=1, column=5)
+        # #
+        Label(master, text="Status:").grid(row=2, column=4)
+        Entry(master, textvariable=self.statusVar).grid(row=2, column=5)
+        # #
+        Label(master, text="Heartbeat:").grid(row=3, column=4)
+        Entry(master, textvariable=self.hbVar).grid(row=3, column=5)
+
+        #
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.recvThread = threading.Thread(target=self.recvFunc)
+        self.recvThread.daemon = True
+        self.recvThread.start()
+
+    def old(self, master):
         self.topFrame = Frame(master)
         self.topFrame.pack(anchor=N,side=LEFT)
         self.commFrame = Frame(self.topFrame)
@@ -43,7 +98,7 @@ class App:
         self.connectButton.pack()
         self.kitFrame = Frame(self.topFrame)
         self.kitFrame.pack(anchor=N,side=LEFT)
-        self.kitName = StringVar();
+        self.kitName = StringVar()
         self.kitName.set("kit.kit")
         self.kitEntry = Entry(self.kitFrame, textvariable=self.kitName)
         self.kitEntry.pack()
@@ -60,26 +115,33 @@ class App:
 
     def connect(self):
         try:
-            self.sock.connect((self.hostEntry.get(), int(self.portEntry.get())))
+            self.sock.connect((self.nameVar.get(), int(self.portVar.get())))
+            self.connected = True
         except IOError as err:
-            print "hitting_hmi:", str(err)
+            print "kitting_hmi:", str(err)
+
+    def disconnect(self):
+        try:
+            self.sock.shutdown(socket.SHUT_RDWR)
+            self.connected = False
+        except IOError as err:
+            print "kitting_hmi:", str(err)
 
     def recvFunc(self):
         while True:
-            try:
-                self.kitName.set(self.sock.recv(1000))
-            except IOError as err:
-                print "hitting_hmi:", str(err)
-                time.sleep(5)
+            if self.connected == True:
+                try:
+                    msg = self.sock.recv(MSG_LEN)
+                except IOError as err:
+                    print "kitting_hmi:", str(err)
+                    time.sleep(PERIOD)
 
     def browse(self):
         filename = askopenfilename()
-        self.kitName.set(filename)
+        self.kitVar.set(filename)
 
 root = Tk()
 
 app1 = App(root)
-# app2 = App(root)
 
 root.mainloop()
-root.destroy()

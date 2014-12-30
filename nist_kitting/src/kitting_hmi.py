5291ce5c310f9a15cb3e1bdde468c5edffaeda34
Fred Proctor <frederick.proctor@nist.gov>
2014-12-30 11:23:40 -0500

    Added tooltip to the kitting HMI

--- nist_kitting/src/kitting_hmi.py
+++ nist_kitting/src/kitting_hmi.py
@@ -4,6 +4,52 @@ import sys, getopt, socket, time, threading, ConfigParser
 from Tkinter import *
 from tkFileDialog import askopenfilename
 
+class ToolTip(object):
+
+    def __init__(self, widget):
+        self.widget = widget
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+
+    def showtip(self, text):
+        "Display text in tooltip window"
+        self.text = text
+        if self.tipwindow or not self.text:
+            return
+        x, y, cx, cy = self.widget.bbox("insert")
+        x = x + self.widget.winfo_rootx() + 27
+        y = y + cy + self.widget.winfo_rooty() +27
+        self.tipwindow = tw = Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x, y))
+        try:
+            # For Mac OS
+            tw.tk.call("::tk::unsupported::MacWindowStyle",
+                       "style", tw._w,
+                       "help", "noActivates")
+        except TclError:
+            pass
+        label = Label(tw, text=self.text, justify=LEFT,
+                      background="#ffffe0", relief=SOLID, borderwidth=1,
+                      font=("tahoma", "8", "normal"))
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
+
+def createToolTip(widget, text):
+    toolTip = ToolTip(widget)
+    def enter(event):
+        toolTip.showtip(text)
+    def leave(event):
+        toolTip.hidetip()
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
+
 INIFILE = ""
 PORT = ""
 HOST = ""
@@ -96,6 +142,7 @@ class App:
         Button(master, text="Browse...", command=self.browse).grid(row=3,column=2)
         self.makeButton = Button(master, text="Make", command=self.make, state=DISABLED)
         self.makeButton.grid(row=3,column=3)
+        createToolTip(self.makeButton, "Send a 'make kit' request")
 
         #
         Label(master, text="Status").grid(row=0, column=4, columnspan=2)

83faf2eadb26ac8d62405dac249cdc53d9d5b79d
Fred Proctor <frederick.proctor@nist.gov>
2015-02-06 15:50:10 -0500

    Refactored DL sim robot interface

--- nist_core/src/crcl_sim_robot_dl.cpp
+++ nist_core/src/crcl_sim_robot_dl.cpp
@@ -1,40 +1,10 @@
-#include <stdio.h>		/* stdin, stderr */
-#include <stddef.h>		/* NULL, sizeof */
+#include <ulapi.h>
+#include "nist_core/crcl.h"
+#include "nist_core/crcl_robot.h"
+#include "nist_core/crcl_sim_robot.h"
+#include "nist_core/crcl_sim_robot_dl.h"
 
-#include <nist_core/crcl.h>
-#include <nist_core/crcl_robot.h>
-#include <nist_core/crcl_sim_robot.h>
-#include <nist_core/crcl_sim_robot_dl.h>
-
-using namespace crcl_robot;
-
-int main(int argc, char *argv[])
-{
-  CrclSimRobot *robot = crcl_sim_robot_new(NULL);
-  double x, y, z, xrot, yrot, zrot;
-
-  x = 1;
-  y = -2.2;
-  z = 3.14;
-  xrot = -0.1;
-  yrot = 0.2;
-  zrot = -0.3;
-
-  crcl_sim_robot_move_straight_to(robot, x, y, z, xrot, yrot, zrot);
-
-  x = 0;
-  y = 0;
-  z = 0;
-  xrot = 0;
-  yrot = 0;
-  zrot = 0;
-
-  crcl_sim_robot_get_robot_pose(robot, &x, &y, &z, &xrot, &yrot, &zrot);
-
-  printf("%f %f %f\n", x, y, z);
-
-  return 0;
-}
+namespace crcl_robot {
 
 // these C-linkage functions make it easier to import as a DLL
 
@@ -88,3 +58,5 @@ ULAPI_DECL_SHARED CanonReturn crcl_sim_robot_get_robot_pose(CrclSimRobot *r, dou
 { // another dummy paren to prevent indenting
 #endif
 }
+
+} // namespace crcl_robot

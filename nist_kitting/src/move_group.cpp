e36f9d93eb0f787e1b4c71c1d6718e6ff1ae74af
Fred Proctor <frederick.proctor@nist.gov>
2016-03-08 16:37:47 -0500

    took out nist_fanuc dependency in nist_fanuc

--- nist_kitting/src/move_group.cpp
+++ nist_kitting/src/move_group.cpp
@@ -1,42 +1,5 @@
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2012, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
-
-/* Author: Sachin Chitta */
 
 #include <ros/ros.h>
-
-// MoveIt!
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_model/robot_model.h>
 #include <moveit/robot_state/robot_state.h>
@@ -47,28 +10,12 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  // BEGIN_TUTORIAL
-  // Start
-  // ^^^^^
-  // Setting up to start using the RobotModel class is very easy. In
-  // general, you will find that most higher-level components will
-  // return a shared pointer to the RobotModel. You should always use
-  // that when possible. In this example, we will start with such a
-  // shared pointer and discuss only the basic API. You can have a
-  // look at the actual code API for these classes to get more
-  // information about how to use more features provided by these
-  // classes.
-  //
-  // We will start by instantiating a
-  // `RobotModelLoader`_
-  // object, which will look up
-  // the robot description on the ROS parameter server and construct a
-  // :moveit_core:`RobotModel` for us to use.
-  //
-  // .. _RobotModelLoader: http://docs.ros.org/api/moveit_ros_planning/html/classrobot__model__loader_1_1RobotModelLoader.html
   robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
   robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
   ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
+
+
+
 
   // Using the :moveit_core:`RobotModel`, we can construct a
   // :moveit_core:`RobotState` that maintains the configuration
--- nist_kitting/src/mover.cpp
+++ nist_kitting/src/mover.cpp
@@ -1,123 +1,32 @@
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2013, SRI International
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of SRI International nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
-
-/* Author: Sachin Chitta */
-
 #include <moveit/move_group_interface/move_group.h>
-#include <moveit/planning_scene_interface/planning_scene_interface.h>
-
 #include <moveit_msgs/DisplayRobotState.h>
 #include <moveit_msgs/DisplayTrajectory.h>
-
 #include <moveit_msgs/AttachedCollisionObject.h>
 #include <moveit_msgs/CollisionObject.h>
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "move_group_interface_tutorial");
+
   ros::NodeHandle node_handle;  
   ros::AsyncSpinner spinner(1);
   spinner.start();
   
   moveit::planning_interface::MoveGroup group("manipulator");
-
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;  
-
-  ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
-  ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
-
-  geometry_msgs::Pose target_pose;
-  target_pose.orientation.w = 1.0;
-  target_pose.position.x = 0.470;
-  target_pose.position.y = 0.000;
-  target_pose.position.z = 0.012;
-  group.setPoseTarget(target_pose);
-
   moveit::planning_interface::MoveGroup::Plan my_plan;
+
+  group.setJointValueTarget(group.getCurrentJointValues());
   bool success = group.plan(my_plan);
-
   ROS_INFO("Cartesian goal: %s", success ? "OK" : "FAILED");    
-  ROS_INFO("moving\n");
-  group.move();
-  ROS_INFO("done\n");
 
-  // Planning to a joint-space goal 
-  std::vector<double> group_variable_values;
-  group.getCurrentState()->copyJointGroupPositions(group.getCurrentState()->getRobotModel()->getJointModelGroup(group.getName()), group_variable_values);
-  
-  // Now, let's modify one of the joints, plan to the new joint
-  // space goal and visualize the plan.
-  group_variable_values[0] = -1.0;  
-  group.setJointValueTarget(group_variable_values);
+  geometry_msgs::PoseStamped current_pose = group.getCurrentPose();
+  ROS_INFO("Current pose is: %f", current_pose.pose.position.x);
+
+  geometry_msgs::Pose target_pose = current_pose.pose;
+  target_pose.position.z += 0.100;
+  group.setPoseTarget(target_pose);
   success = group.plan(my_plan);
-
-  ROS_INFO("Joint space goal: %s",success?"":"FAILED");
-
-  ROS_INFO("moving\n");
-  group.move();
-  ROS_INFO("done\n");
-
-  // Cartesian Paths
-  // ^^^^^^^^^^^^^^^
-  // You can plan a cartesian path directly by specifying a list of waypoints 
-  // for the end-effector to go through. Note that we are starting 
-  // from the new start state above.  The initial pose (start state) does not
-  // need to be added to the waypoint list.
-  std::vector<geometry_msgs::Pose> waypoints;
-
-  geometry_msgs::Pose target_pose3 = target_pose;
-  target_pose3.position.x += 0.2;
-  target_pose3.position.z += 0.2;
-  waypoints.push_back(target_pose3);  // up and out
-
-  target_pose3.position.y -= 0.2;
-  waypoints.push_back(target_pose3);  // left
-
-  target_pose3.position.z -= 0.2;
-  target_pose3.position.y += 0.2;
-  target_pose3.position.x -= 0.2;
-  waypoints.push_back(target_pose3);  // down and right (back to start)
-
-  // We want the cartesian path to be interpolated at a resolution of 1 cm
-  // which is why we will specify 0.01 as the max step in cartesian
-  // translation.  We will specify the jump threshold as 0.0, effectively
-  // disabling it.
-  moveit_msgs::RobotTrajectory trajectory;
-  double fraction = group.computeCartesianPath(waypoints,
-                                               0.01,  // eef_step
-                                               0.0,   // jump_threshold
-                                               trajectory);
+  ROS_INFO("Cartesian goal: %s", success ? "OK" : "FAILED");    
 
   ROS_INFO("moving\n");
   group.move();
